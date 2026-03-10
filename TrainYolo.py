1856a51c65e1e4b21fe069d5964526d1c0e63b2a
Salvatore Casalena <salcasalena@gmail.com>
2026-03-10 18:06:31 -0400

    Trained Models

--- TrainYolo.py
+++ TrainYolo.py
@@ -219,8 +219,8 @@ def parse_args():
 
     parser.add_argument("--epochs", type=int, default=3, help="Number of training epochs")
     parser.add_argument("--imgsz", type=int, default=320, help="Training image size")
-    parser.add_argument("--batch", type=int, default=16, help="Batch size")
-    parser.add_argument("--workers", type=int, default=0, help="Number of dataloader workers")
+    parser.add_argument("--batch", type=int, default=32, help="Batch size")
+    parser.add_argument("--workers", type=int, default=2, help="Number of dataloader workers")
     parser.add_argument("--cache", type=str, default="disk", help="Dataset cache mode")
 
     parser.add_argument(
